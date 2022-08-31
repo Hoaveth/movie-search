a8913f508995f5d093042aa49f40057a2eebd1b3
Hoaveth <hoaveth.gaming@gmail.com>
2022-08-31 21:39:58 +0800

    Added functionalities search and display of movie

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { IMDB_API_KEY } from "./utils/constant";
+import SearchIcon from "./assets/search.svg";
+import Card from "./components/Card";
+import "./App.css";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [title, setTitle] = useState("");
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${IMDB_API_KEY}s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Movieland</h1>
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search for movies"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          onClick={() => searchMovies(title)}
+          alt="search"
+        />
+      </div>
+      <div className="container">
+        {movies ? (
+          movies.map((movie) => <Card movie={movie} key={movie.imdbID} />)
+        ) : (
+          <h1>No movies found.</h1>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/utils/constant.js
@@ -0,0 +1,2 @@
+export const IMDB_API_KEY =
+  "http://www.omdbapi.com/?i=tt3896198&apikey=19302189&";
